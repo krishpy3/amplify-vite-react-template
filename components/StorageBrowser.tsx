48e812c3b40f9da86bc59de24a458b72aa61cbc8
Krish <98233937+krishpy3@users.noreply.github.com>
2025-03-04 23:04:01 +0530

    Update StorageBrowser.tsx

--- components/StorageBrowser.tsx
+++ components/StorageBrowser.tsx
@@ -1,12 +1,27 @@
-import { createAmplifyAuthAdapter, createStorageBrowser } from '@aws-amplify/ui-react-storage/browser';
+import {
+    createManagedAuthAdapter,
+    createStorageBrowser,
+} from '@aws-amplify/ui-react-storage/browser';
 import '@aws-amplify/ui-react-storage/styles.css';
-import { Amplify } from 'aws-amplify';
-import config from '../amplify_outputs.json';
 
-// Configure Amplify using the imported configuration
-Amplify.configure(config);
-
-// Create the StorageBrowser component with Amplify authentication
 export const { StorageBrowser } = createStorageBrowser({
-  config: createAmplifyAuthAdapter(),
+   config: createManagedAuthAdapter({
+    credentialsProvider: async (options?: { forceRefresh?: boolean }) => {
+      // return your credentials object
+      return {
+        credentials: {
+          accessKeyId: 'my-access-key-id',
+          secretAccessKey: 'my-secret-access-key',
+          sessionToken: 'my-session-token',
+          expiration: new Date(),
+        },
+      }
+    },
+    // AWS `region` and `accountId` of the S3 Access Grants Instance.
+    region: 'us-east-1',
+    accountId: '471112784336',
+    // call `onAuthStateChange` when end user auth state changes 
+    // to clear sensitive data from the `StorageBrowser` state
+    registerAuthListener: (onAuthStateChange) => {},
+  })
 });
